9d70444ff6f446e990648e2921b03e72a747a3d5
Reiner Marquez <rmaceissoft@gmail.com>
2015-01-31 20:46:10 -0500

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='python-guidestar',
-      version='0.1',
+      version='0.1.1',
       description='Python client library for Guidestar API',
       url='http://github.com/rmaceissoft/python-guidestar',
       author='Reiner Marquez',
